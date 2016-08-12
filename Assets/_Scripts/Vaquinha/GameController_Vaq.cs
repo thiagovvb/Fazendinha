0e3eaf796f94887d9f13400886b37620af924acf
Thiago Vieira <thiagovvb@gmail.com>
2016-08-12 09:41:50 -0300

    Vaquinha mecânica completa

--- Assets/_Scripts/Vaquinha/GameController_Vaq.cs
+++ Assets/_Scripts/Vaquinha/GameController_Vaq.cs
@@ -6,6 +6,7 @@ public class GameController_Vaq : MonoBehaviour {
 
 	private int state;
 	private int currentSeq;
+	private int hitSeq;
 	private float lightsStartTime;
 	private List<int> ordem = new List<int>();
 	public GameObject[] luzes;
@@ -20,17 +21,18 @@ public class GameController_Vaq : MonoBehaviour {
 	void Update () {
 	
 		if(state == 0){
-			
-			int newValue = Random.Range(1,5);
-			Debug.Log("Valor gerado = " + newValue);
+
+			int newValue = Random.Range(0,4);
+			Debug.Log("Valor gerado = " + (newValue + 1));
 			ordem.Add(newValue);
 			state = 1;
-			currentSeq = -1;
+			currentSeq = 0;
 
 		}else if(state == 1){
 			
-			if(++currentSeq >= ordem.Count){
-				state = 0;
+			if(currentSeq >= ordem.Count){
+				hitSeq = 0;
+				state = 4;
 			}else{
 				lightsStartTime = Time.time;
 				luzes[ordem[currentSeq]].GetComponent<Light>().enabled = true;
@@ -38,14 +40,66 @@ public class GameController_Vaq : MonoBehaviour {
 			}
 
 		}else if(state == 2){
-		
-			if(Time.time - lightsStartTime > 3){
-				luzes[ordem[currentSeq]].GetComponent<Light>().enabled = false;
-				state = 1;
+
+			if(Time.time - lightsStartTime > 1){
+				luzes[ordem[currentSeq++]].GetComponent<Light>().enabled = false;
+				state = 3;
 			}
 		
 		}else if(state == 3){
-			Debug.Log("Acabou");
+			if(Time.time - lightsStartTime > 1.5f){
+				state = 1;
+			}
+		}else if(state == 4){
+
+			if(hitSeq >= ordem.Count) state = 0;
+
+			if(Input.GetKeyDown("1")){
+				if(ordem[hitSeq] == 0){
+					state = 5;
+					lightsStartTime = Time.time;
+					luzes[ordem[hitSeq]].GetComponent<Light>().enabled = true;
+				}
+				else state = 6;
+			}
+			if(Input.GetKeyDown("2")){
+				if(ordem[hitSeq] == 1){
+					state = 5;
+					lightsStartTime = Time.time;
+					luzes[ordem[hitSeq]].GetComponent<Light>().enabled = true;
+				}
+				else state = 6;
+			}
+			if(Input.GetKeyDown("3")){
+				if(ordem[hitSeq] == 2){
+					state = 5;
+					lightsStartTime = Time.time;
+					luzes[ordem[hitSeq]].GetComponent<Light>().enabled = true;
+				}
+				else state = 6;
+			}
+			if(Input.GetKeyDown("4")){
+				if(ordem[hitSeq] == 3){
+					state = 5;
+					lightsStartTime = Time.time;
+					luzes[ordem[hitSeq]].GetComponent<Light>().enabled = true;
+				}
+				else state = 6;
+			}
+				
+
+		}else if(state == 5){
+			if(Time.time - lightsStartTime > 0.75 && luzes[ordem[hitSeq]].GetComponent<Light>().enabled){
+				luzes[ordem[hitSeq]].GetComponent<Light>().enabled = false;
+			}
+
+			if(Time.time - lightsStartTime > 1.25){
+				hitSeq++;
+				state = 4;
+			}
+
+		}else if(state == 6){
+			Debug.Log("perdeu!");
 		}
 
 	}
