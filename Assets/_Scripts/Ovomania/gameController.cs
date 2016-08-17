2258d290a79494daeda69dabca1c275a7b49c2f6
Thiago Vieira <thiagovvb@gmail.com>
2016-08-17 17:41:05 -0300

    Jogo dos ovos
    Cenario quase pronto

---
+++ Assets/_Scripts/Ovomania/gameController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameController : MonoBehaviour {
+
+	public Transform ovoBranco;
+	private Vector3[] positions;
+	private Quaternion defaultQuaternion;
+	private float startTime;
+
+	// Use this for initialization
+	void Start () {
+		
+		defaultQuaternion = Quaternion.Euler(new Vector3(90,-90,0));
+
+		startTime = Time.time;
+
+		positions = new Vector3[4];
+
+		positions[0] = new Vector3(2.65f, 3.13f, -6.06f);
+		positions[1] = new Vector3(1.32f, 3.13f, -6.06f);
+		positions[2] = new Vector3(0.13f, 3.13f, -6.06f);
+		positions[3] = new Vector3(-1.14f, 3.13f, -6.06f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		int num;
+		if(Time.time - startTime > 1){
+			for(int i = 0; i < 4; i++){
+
+				num = Random.Range(0,100);
+				if(num >= 80 && num <= 100){
+					Instantiate(ovoBranco,positions[i],defaultQuaternion);
+				}
+
+			}
+			startTime = Time.time;
+		}
+
+	}
+}
---
+++ Assets/_Scripts/Ovomania/moveOvo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveOvo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (transform.position.z < -0.3) {
+			Debug.Log("nhe");
+			transform.RotateAround(transform.position,transform.up, -Time.deltaTime * 90);
+			transform.Translate(new Vector3(0,0,Time.deltaTime), Space.World);
+		}
+
+	}
+}
